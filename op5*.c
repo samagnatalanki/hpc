12dd9269309096c2cd6033dd4f2fdcbb84494d92
samagnatalanki <31427759+samagnatalanki@users.noreply.github.com>
2018-12-07 07:03:43 +0530

    Update op5*.c

--- op5*.c
+++ op5*.c
@@ -1,45 +1,35 @@
-// NOT RUNNING ON DIFFERENT THREADS
 
 #include<stdio.h>
-#include<stdlib.h>
 #include<omp.h>
+#include<stdlib.h>
 
-int main(int argc,char *argv[])
-{
-    int i,tid,nth;
-    float a[10],b[10],c[10],d[10];
+omp_lock_t lock;
 
-    for (i=0;i<10;i++)
-    {
-        a[i]=i*2.0;
-        b[i]=i+1.0;
-        c[i]=d[i]=0;
-    }
+int main(){
 
-    #pragma omp parallel shared(b,c,nth,d,a) private(tid,i)
-    {
-        tid=omp_get_thread_num();
-        printf("thread %d starting.... \n",tid);
-        #pragma omp sections nowait
-        {
-            #pragma omp section
-            {
-                printf("thread doing 1 - %d \n",tid);
-                for(i=0;i<10;i++)
-                {
-                    c[i]=a[i]+b[i];
-                    printf("thread %d : %f + %f = %f \n",tid,a[i],b[i],c[i]);
-                }
-            }
-            #pragma omp section
-            {
-                printf("thread doing 2 - %d \n",tid);
-                for(i=0;i<10;i++)
-                {
-                    d[i]=a[i]-b[i];
-                    printf("thread %d : %f + %f = %f \n",tid,a[i],b[i],d[i]);
-                }
-            }
-        }
-    }
+	int a[]={1,2,3,4,5},b[]={1,2,3,4,5},c[5],d[5],i,j;
+	
+	#pragma omp parallel shared(a,b,c,d) private(i)
+	
+		#pragma omp sections nowait
+
+			#pragma section
+			{
+				for(i=0;i<5;i++){
+					c[i] = a[i] + b[i];
+				printf("\n Section 1 : Thread %d el %d",omp_get_thread_num(),i);
+				}
+			
+			}
+
+			#pragma section
+			{
+				for(i=0;i<5;i++){
+					d[i] = a[i] * b[i];
+				printf("\n Section 2 : Thread %d el %d",omp_get_thread_num(),i);
+				}
+			
+			}			
+		
+	return 0;
 }
